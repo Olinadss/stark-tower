3343807518422345502b00c9b26800ba2619d0d3
Danilo Santos <danilo.santossouza@outlook.com>
2025-08-20 10:41:07 -0300

    feat: configura layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,29 @@
 import { createBrowserRouter } from "react-router";
 import { Dashboard } from "../pages/app/Dashboard";
 import { SignIn } from "../pages/auth/sign-in";
+import { AppLayout } from "../pages/app/_layouts/app";
+import { AuthLayout } from "../pages/app/_layouts/auth";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+    ],
   },
+
   {
     path: "/sign-in",
-    element: <SignIn />,
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+    ],
   },
 ]);
