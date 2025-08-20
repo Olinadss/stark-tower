5102954fcba345a5d2a14aae7ad5e1fef59fdba4
Danilo Souza <danilo.souza@bmsprojetos.com>
2025-08-20 10:11:09 -0300

    feat: cria header e configura o react router dom

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <span>Stark Tower</span>
+      <span>Tony Stark</span>
+    </Container>
+  );
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -21,9 +21,15 @@ export const GlobalStyle = createGlobalStyle`
     background-color: #f0f0f0;
   }
 
+  body,input, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
   h1, h2, h3, h4, h5, h6 {
     margin: 0;
     padding: 0;
+    font-weight: 600;
   }
 
   p {
